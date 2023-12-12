5672ef5efbcb9294831a3895a70ddbee5dd30983
amschel99 <6290.2020@students.ku.ac.ke>
2023-12-12 13:58:47 +0300

    pushing

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 
 import qrcode from "qrcode-terminal"
-import { Configuration,OpenAIApi } from "openai";
+
 import { Client } from "whatsapp-web.js";
 
 import dotenv from "dotenv"
@@ -10,22 +10,7 @@ dotenv.config()
 
             
 const client = new Client();
-const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
-  const askAi= async (prompt)=>{
-    const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        temperature: 0.5,
-        max_tokens: 1000,
-        top_p: 1,
-       
-        n:'1'
-      });
-      return response.data.choices[0].text;
-  }
+
 
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
@@ -41,8 +26,8 @@ client.on('ready', () => {
 client.on("message", async(message)=>{
     
 try{
-const answer=await askAi(message.body)
-client.sendMessage(message.from, answer)
+
+client.sendMessage("hi brodda")
 
 }
 catch(e){
